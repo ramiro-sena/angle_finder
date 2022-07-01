fe0e1d1cd5b28a940158def03598b215ffadab08
Ramiro <ramiro.volt@gmail.com>
2022-07-01 09:54:38 -0300

    test with pipenv

--- pages/api/hellow.py
+++ pages/api/hellow.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from cowpy import cow
 
 class handler(BaseHTTPRequestHandler):
-
+  
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
